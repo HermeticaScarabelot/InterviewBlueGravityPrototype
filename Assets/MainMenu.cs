e585175933345d1212f3dd98c2957ba1d53880e4
HermeticaScarabelot <hermeticascarabelot@gmail.com>
2023-10-22 03:46:04 -0300

    Finished final Tweaks - Final

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameObject optionsGo;
+    [SerializeField] private float tweenDuration;
+    [SerializeField] private Ease tweenEase;
+    [SerializeField] private bool isTweening;
+    public void StartGame()
     {
+        SceneManager.LoadScene("Main Game", LoadSceneMode.Single);
+    }
+
+    public void OpenOptions()
+    {
+        if (isTweening)
+        {
+            return;
+        }
+
+        optionsGo.transform.DOKill();
+        isTweening = true;
+        optionsGo.transform.localScale = Vector3.zero;
+        optionsGo.SetActive(true);
+        optionsGo.transform.DOScale(1, tweenDuration).SetEase(tweenEase).OnComplete(() => isTweening = false);
+    }
+
+    public void CloseOptions()
+    {
+        if (isTweening)
+        {
+            return;
+        }
+
+        isTweening = true;
+        optionsGo.transform.DOScale(0, tweenDuration).SetEase(tweenEase).OnComplete(() => isTweening = false);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
+        Application.Quit();
+    }
+
+    public void ChangeScreenMode(bool fullscreen)
+    {
+        if (fullscreen)
+        {
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            Screen.fullScreen = false;
+        }
+    }
+
+    public void UpdateResolution(int index)
+    {
+        Vector2Int resolution = new Vector2Int(0, 0);
+        switch (index)
+        {
+            case 0:
+                resolution.x = 1366;
+                resolution.y = 768;
+                break;
+            case 1:
+                resolution.x = 1600;
+                resolution.y = 900;
+                break;
+            case 2:
+                resolution.x = 1920;
+                resolution.y = 1080;
+                break;
+            case 3:
+                resolution.x = 2560;
+                resolution.y = 1440;
+                break;
+            case 4:
+                resolution.x = 3840;
+                resolution.y = 2160;
+                break;
+        }
+        Screen.SetResolution(resolution.x, resolution.y, Screen.fullScreen);
         
     }
 }
